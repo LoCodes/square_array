bc3080016f99b144249b168f2aa46f70fc835f6c
Loren Cipriano <loren25cipriano@gmail.com>
2020-11-18 13:35:50 -0800

    commit tree 7130dd8d283459a244aaceb00101d0a220199652

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,21 @@
 def square_array(array)
   # your code here
+  new_arr = []
+  array.each do |i| 
+    new_arr << i ** 2 
+  end 
+  new_arr
 end
+
+# same as above, cleaner method
+
+# def square_array(array)
+#   new_arr = []
+#   array.each { |i| new_arr << i ** 2 }
+#   new_arr
+# end
+
+# #square_array
+#  does not call on collect/map or inject
+#  calls on each
+#  should square the elements in an array
